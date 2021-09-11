b358656ec34f48b2c13e1f7b6f9fe0ef7069b917
hw19930119 <493619275@qq.com>
2021-09-11 11:42:36 +0800

    通用工具类提出

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -4,10 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -20,7 +17,7 @@ public class OrderController {
     private  RestTemplate restTemplate;
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
-        CommonResult commonResult = restTemplate.patchForObject(payment_url + "/payment/create", payment, CommonResult.class);
+        CommonResult commonResult = restTemplate.postForObject(payment_url + "/payment/create", payment, CommonResult.class);
         return commonResult;
     }
     @GetMapping("/consumer/payment/getPayment/{id}")
